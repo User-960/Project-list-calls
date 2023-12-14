00fd27d66d95e7c363ca2df99bef282804ef5d5e
Ivan <ivanvolodin960@gmail.com>
2023-12-15 00:56:27 +0300

    feat: add styles for item in list of calls

--- src/components/screens/home/listCalls/ListCalls.tsx
+++ src/components/screens/home/listCalls/ListCalls.tsx
@@ -1,23 +1,78 @@
+import Image from 'next/image'
 import React from 'react'
 
+import avatarOne from '../../../../../public/images/avatarOne.jpg'
+import outgoingCall from '../../../../../public/images/outgoingCall.svg'
+
 import styles from './ListCalls.module.scss'
+
+const cn = require('clsx')
 
 const ListCalls = () => {
 	return (
 		<>
 			<div className={styles.listCallsWrapper}>
 				<div className={styles.navList}>
-					<div className={styles.navListColumn}>Тип</div>
-					<div className={styles.navListColumn}>Время</div>
-					<div className={styles.navListColumn}>Сотрудник</div>
-					<div className={styles.navListColumn}>Звонок</div>
-					<div className={styles.navListColumn}>Источник</div>
-					<div className={styles.navListColumn}>Оценка</div>
-					<div className={styles.navListColumn}>Длительность</div>
+					<div className={cn(styles.navListColumn, styles.navListType)}>
+						Тип
+					</div>
+					<div className={cn(styles.navListColumn, styles.navListTime)}>
+						Время
+					</div>
+					<div className={cn(styles.navListColumn, styles.navListEmployee)}>
+						Сотрудник
+					</div>
+					<div className={cn(styles.navListColumn, styles.navListCall)}>
+						Звонок
+					</div>
+					<div className={cn(styles.navListColumn, styles.navListSource)}>
+						Источник
+					</div>
+					<div className={cn(styles.navListColumn, styles.navListEstimation)}>
+						Оценка
+					</div>
+					<div className={cn(styles.navListColumn)}>Длительность</div>
 				</div>
 
 				<ul className={styles.listCalls}>
-					<li className={styles.listCallsItem}></li>
+					<li className={styles.listCallsItem}>
+						<div className={styles.itemType}>
+							<Image
+								className={styles.itemTypeCallIcon}
+								src={outgoingCall}
+								alt='type of call'
+								draggable={false}
+							/>
+						</div>
+
+						<div className={styles.itemTime}>19:00</div>
+
+						<div className={styles.itemEmployee}>
+							<Image
+								className={styles.itemAvatar}
+								src={avatarOne}
+								alt='type of call'
+								draggable={false}
+							/>
+						</div>
+
+						<div className={styles.itemCall}>+7 (987) 567-17-12</div>
+
+						<div className={styles.itemSource}>Rabota.ru</div>
+
+						<div className={styles.itemEstimation}>
+							<div
+								className={cn(
+									styles.itemEstimationBlock,
+									styles.itemEstimationSuccess
+								)}
+							>
+								Отлично
+							</div>
+						</div>
+
+						<div className={styles.itemDuration}>12:06</div>
+					</li>
 				</ul>
 			</div>
 		</>
