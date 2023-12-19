4e3dd479b3ac7202e5baaf994be20a284c97f932
Ivan <ivanvolodin960@gmail.com>
2023-12-19 20:14:28 +0300

    feat: add hook useOnClickOutside

--- src/components/screens/home/filter/filterDate/FilterDate.tsx
+++ src/components/screens/home/filter/filterDate/FilterDate.tsx
@@ -1,9 +1,6 @@
-import Image from 'next/image'
 import React, { useState } from 'react'
 
-import { useListCalls } from '@/components/hooks/useListCalls'
-
-import iconCalendar from '../../../../../../public/images/iconCalendar.svg'
+import { useOnClickOutside } from '@/components/hooks/useOnClickOutside'
 
 import styles from './FilterDate.module.scss'
 import FilterOptions from './filterOptions/FilterOptions'
@@ -11,40 +8,33 @@ import FilterOptions from './filterOptions/FilterOptions'
 const cn = require('clsx')
 
 const FilterDate = () => {
-	const [openFilterOptions, setOpenFilterOptions] = useState<boolean>(false)
 	const [dateCount, setDateCount] = useState<string>('3 дня')
+	const { ref, isShow, setIsShow } = useOnClickOutside(false)
 
 	return (
 		<>
-			<div className={styles.filterDays}>
-				{openFilterOptions && (
+			<div className={styles.filterDays} ref={ref}>
+				{isShow && (
 					<FilterOptions
-						closeFilterOptions={() => setOpenFilterOptions(false)}
+						closeFilterOptions={() => setIsShow(false)}
 						dateCount={dateCount}
 						setDateCount={setDateCount}
 					/>
 				)}
 				<button
 					className={cn(styles.filterBtn, styles.filterLeftBtn)}
-					onClick={() => setOpenFilterOptions(prev => !prev)}
+					onClick={() => setIsShow(prev => !prev)}
 				></button>
 				<button
 					className={styles.daysBtn}
-					onClick={() => setOpenFilterOptions(prev => !prev)}
+					onClick={() => setIsShow(prev => !prev)}
 				>
-					{/* <Image
-						src={iconCalendar}
-						alt='icon of calendar'
-						width={16}
-						height={18}
-						draggable={false}
-					/> */}
 					<div className={styles.iconCalendar}></div>
 					<p className={styles.daysMount}>{dateCount}</p>
 				</button>
 				<button
 					className={cn(styles.filterBtn, styles.filterRightBtn)}
-					onClick={() => setOpenFilterOptions(prev => !prev)}
+					onClick={() => setIsShow(prev => !prev)}
 				></button>
 			</div>
 		</>
